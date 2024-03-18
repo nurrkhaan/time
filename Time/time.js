23285ebca85f65dba5b7fa3d2b9f7dc6c53cc837
nurrkhaan <163830917+nurrkhaan@users.noreply.github.com>
2024-03-18 14:06:00 +0500

    Add files via upload

---
+++ Time/time.js
@@ -0,0 +1,41 @@
+//const now = new Date();
+
+let mode = "full";
+//const Now = new Date();
+const output = document.getElementById("output");
+const fullBtn = document.getElementById("full");
+const dateBtn = document.getElementById("date");
+const timeBtn = document.getElementById("time");
+
+function bindMode(name) {
+  return function () {
+    mode = name;
+    update();
+  };
+}
+
+fullBtn.onclick = bindMode("full");
+dateBtn.onclick = bindMode("date");
+timeBtn.onclick = bindMode("time");
+
+setInterval(update, 100);
+update();
+
+function update() {
+  output.textContent = format(mode);
+}
+
+function format(formatMode) {
+  const now = new Date();
+
+  switch (formatMode) {
+    case "time":
+      return now.toLocaleTimeString() + "." + now.getMilliseconds();
+    case "date":
+      return now.toLocaleDateString();
+    case "full":
+      return now.toLocaleDateString() + " " + now.toLocaleTimeString();
+    default:
+      return now.toLocaleTimeString();
+  }
+}
